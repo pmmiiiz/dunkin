fe2a34783a3479acb6b167be927871813391367a
Maousaka <62070128@it.kmitl.ac.th>
2020-10-19 18:55:01 +0700

    A...

---
+++ script.js
@@ -0,0 +1,18 @@
+mybutton = document.getElementById("MyBTN");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 100000  || document.documentElement.scrollTop > 1250) {
+    MyBTN.style.display = "block";
+  } else {
+    MyBTN.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
