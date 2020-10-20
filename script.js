54c5b031cf3ea3eb8326110fa9b393e15166398e
engchannnnn <cchankrachang52@gmail.com>
2020-10-20 17:46:58 +0700

    load

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 mybutton = document.getElementById("MyBTN");
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () { scrollFunction() };
 
 function scrollFunction() {
-  if (document.body.scrollTop > 100000  || document.documentElement.scrollTop > 1250) {
+  if (document.body.scrollTop > 100000 || document.documentElement.scrollTop > 1250) {
     MyBTN.style.display = "block";
   } else {
     MyBTN.style.display = "none";
@@ -16,3 +16,13 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+// Pre-Load And Iframe Delay
+setTimeout(function(){
+  $('#loader-bg').fadeToggle("fast");
+  }, 250);
+  function load() {
+  setTimeout(function(){
+    document.querySelector('#embeded').style.display = 'block';
+  }, 1500);
+  }
+
